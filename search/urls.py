9d4d9b57b30cdf798baa9aa568f76ffd20b68ac0
unknown <henry.alakazhang@gmail.com>
2015-09-13 17:22:28 +1000

    added search by location name with autocomplete

--- search/urls.py
+++ search/urls.py
@@ -3,8 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #eg. /search/tags/
-    url(r'^tags/$', views.tag_search, name='tag_search'),
-    #eg. /results/tags/
-    url(r'^tags/results/$', views.tag_search_results, name='tag_search_results'),
+    #ie. /search/tags/
+    url(r'^tags/?$', views.tag_search, name='tag_search'),
+    #ie. /search/tags/results/
+    url(r'^tags/results/?$', views.tag_search_results, name='tag_search_results'),
+    
+    #ie. /search/location/
+    url(r'^location/?$', views.location_search_text, name='location_search_text`'),
+    #ie. /search/location/results/
+    url(r'^location/results/?$', views.location_search_text_results, name='location_search_text_results'),
 ]
--- search/views.py
+++ search/views.py
@@ -6,3 +6,10 @@ def tag_search(request):
 def tag_search_results(request):
    context = {'tags' : request.POST['tags'].split(';')}
    return render(request, 'search/tag_search_results.html', context)
+   
+def location_search_text(request):
+    return render(request, 'search/location_search_text.html')
+
+def location_search_text_results(request):
+    context = {'location' : request.POST['location']}
+    return render(request, 'search/location_search_text_results.html', context)
