cbe213a52e2017a5509b7d3bc8e01556a621078c
Benjamin Mo <b.mo96@hotmail.com>
2015-09-14 12:08:09 +1000

    fixed some currently unused code for future use (JS Async get request)

--- search/urls.py
+++ search/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #ie. /search/tags/
-    url(r'^tags/?$', views.tag_search, name='tag_search'),
-    #ie. /search/tags/results/
-    url(r'^tags/results/?$', views.tag_search_results, name='tag_search_results'),
-    
-    #ie. /search/location/
-    url(r'^location/?$', views.location_search_text, name='location_search_text`'),
-    #ie. /search/location/results/
-    url(r'^location/results/?$', views.location_search_text_results, name='location_search_text_results'),
+   # /search/tags/
+   url(r'^tags/?$', views.tag_search, name='tag_search'),
+   # /search/tags/results/
+   url(r'^tags/results/?$', views.tag_search_results, name='tag_search_results'),
+   # /search/map/
+   url(r'^map/?$', views.location_search, name='location_search'),
+   # /search/map/results
+   url(r'^map/results.*', views.location_search_results, name='location_search_results'),
+   # /search/location/
+   url(r'^location/?$', views.location_search_text, name='location_search_text`'),
+   # /search/location/results/
+   url(r'^location/results/?$', views.location_search_text_results, name='location_search_text_results'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 
 def tag_search(request):
-    return render(request, 'search/tag_search.html')
+   return render(request, 'search/tag_search.html')
     
 def tag_search_results(request):
    context = {'tags' : request.POST['tags'].split(';')}
    return render(request, 'search/tag_search_results.html', context)
    
+def location_search(request):
+	return render(request, 'search/location_search.html')
+
+def location_search_results(request):
+	context = {}
+	context["lat"] = request.GET.get('lat', '')
+	context["lng"] = request.GET.get('lng', '')
+	return render(request, 'search/location_search_results.html', context)
+
 def location_search_text(request):
-    return render(request, 'search/location_search_text.html')
+   return render(request, 'search/location_search_text.html')
 
 def location_search_text_results(request):
-    context = {'location' : request.POST['location']}
-    return render(request, 'search/location_search_text_results.html', context)
+   context = {'location' : request.POST['location']}
+   return render(request, 'search/location_search_text_results.html', context)
