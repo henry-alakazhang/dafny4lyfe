827700d8414c6f34028870501f60de0a15c0fa59
Benjamin Mo <b.mo96@hotmail.com>
2015-09-15 13:11:29 +1000

    added rudimentary flickr API integration (not tested)

--- search/views.py
+++ search/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import flickrapi
+
+FLICKR_API_KEY = u'd924f5ea2a765922fc8794b3f9942133'
+FLICKR_API_SECRET = u'2eefb6d5fbaab4f4'
+flickr = flickrapi.FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET)
 
 def tag_search(request):
    return render(request, 'search/tag_search.html')
@@ -8,13 +13,24 @@ def tag_search_results(request):
    return render(request, 'search/tag_search_results.html', context)
    
 def location_search(request):
-	return render(request, 'search/location_search.html')
+   return render(request, 'search/location_search.html')
 
 def location_search_results(request):
-	context = {}
-	context["lat"] = request.GET.get('lat', '')
-	context["lng"] = request.GET.get('lng', '')
-	return render(request, 'search/location_search_results.html', context)
+   context = {}
+   lat = float(request.GET.get('lat', ''))
+   lng = float(request.GET.get('lng', ''))
+   minLat = lat - 1
+   maxLat = lat + 1
+   minLng = lng - 1
+   maxLng = lng + 1
+
+   context["lat"] = lat
+   context["lng"] = lng
+   bounds = "{0}, {1}, {2}, {3}".format(minLng, minLat, maxLng, maxLat)
+
+#   results = flickr.photos.search(bbox=bounds, tags='beach')
+#   print(results)
+   return render(request, 'search/location_search_results.html', context)
 
 def location_search_text(request):
    return render(request, 'search/location_search_text.html')
