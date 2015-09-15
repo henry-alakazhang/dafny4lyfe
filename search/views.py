0c979cc762675006ffb17cefe0f5668b8b3a077e
Benjamin Mo <b.mo96@hotmail.com>
2015-09-16 00:00:03 +1000

    added image displays on results page (WIP)

--- search/views.py
+++ search/views.py
@@ -3,6 +3,7 @@ import flickrapi
 
 FLICKR_API_KEY = u'd924f5ea2a765922fc8794b3f9942133'
 FLICKR_API_SECRET = u'2eefb6d5fbaab4f4'
+
 flickr = flickrapi.FlickrAPI(FLICKR_API_KEY, FLICKR_API_SECRET)
 
 def tag_search(request):
@@ -19,17 +20,25 @@ def location_search_results(request):
    context = {}
    lat = float(request.GET.get('lat', ''))
    lng = float(request.GET.get('lng', ''))
-   minLat = lat - 1
-   maxLat = lat + 1
-   minLng = lng - 1
-   maxLng = lng + 1
+   minLat = lat - 0.2
+   maxLat = lat + 0.2
+   minLng = lng - 0.2
+   maxLng = lng + 0.2
 
    context["lat"] = lat
    context["lng"] = lng
    bounds = "{0}, {1}, {2}, {3}".format(minLng, minLat, maxLng, maxLat)
 
-#   results = flickr.photos.search(bbox=bounds, tags='beach')
-#   print(results)
+   urlList = {}
+   for photo in flickr.walk(bbox=bounds, tags='beach, sky, sunset', tag_mode='all'):
+      #https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+      photoUrl = ('https://farm' + photo.get('farm') + 
+                  '.staticflickr.com/' + photo.get('server') + '/' + 
+                  photo.get('id') + '_' + photo.get('secret') + '.jpg')
+      urlList[photoUrl] = {}
+      print(photoUrl)
+   
+   context['urlList'] = urlList
    return render(request, 'search/location_search_results.html', context)
 
 def location_search_text(request):
