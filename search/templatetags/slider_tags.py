4d314d540258c63fe83f932dafda434c59ef07c1
Damon <damon.ho@student.unsw.edu.au>
2015-09-23 09:40:49 +1000

    Date added to Slider
    Added selected date

--- search/templatetags/slider_tags.py
+++ search/templatetags/slider_tags.py
@@ -1,12 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django import template
+import sys
 
 register = template.Library()
 
 @register.simple_tag
-def get_current_time(format_string):
-   return datetime.datetime.now().strftime(format_string)
-
-@register.simple_tag
 def time_from_start():
    return (datetime.today() - datetime(1850,1,1)).days
+   
