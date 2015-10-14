af5b2f7812839536612aa9a11d7c0fc51be30d75
Benjamin Mo <b.mo96@hotmail.com>
2015-10-15 03:58:38 +1100

    initial async flickr results working, first page only

--- static/search/js/flickr.js
+++ static/search/js/flickr.js
@@ -1,8 +1,6 @@
 jQuery(function() {
-  // Flickr Key + Secret
+  // Flickr Key
   var apiKey = 'd924f5ea2a765922fc8794b3f9942133';
-  var apiSecret = '2eefb6d5fbaab4f4';
-  window.alert("helo");
 
   jQuery('<img alt="" />').attr('id', 'loader').attr('src', 'ajax-loader.gif').appendTo('#image-container');
 
@@ -17,7 +15,44 @@ jQuery(function() {
     }
     return(false);
   }
-  //"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=YOURAPIKEYHERE&tags=yokota+air+base&safe_search=1&per_page=20"  
 
+  function search() {
+    var baseUrl = 'https://api.flickr.com/services/rest/?jsoncallback=?';
+    var flickrOptions = {
+      method: 'flickr.photos.search',
+      api_key: apiKey,
+      tags: getQueryVar('tags'),
+      min_taken_date: getQueryVar('min_date'),
+      max_taken_date: getQueryVar('max_date'),
+      lat: getQueryVar('lat'),
+      lon: getQueryVar('lng'),
+      radius: getQueryVar('dist'),
+      tag_mode: 'all',
+      sort: 'date-taken-desc',
+      extras: 'date_taken',
+      format: 'json',
+      per_page: '200'
+    }
+
+    jQuery.getJSON(baseUrl, flickrOptions, function(data) {
+      jQuery.each(data.photos.photo, function(index, item) {
+        var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + 
+                       item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
+        console.log(photoURL);
+        var imgCont = "<figure> <img src='" + photoURL + "'></figure>";
+        jQuery(imgCont).appendTo('#image-container');
+      });
+    });
+
+/*    "http://api.flickr.com/services/rest/?method=flickr.photos.search" +
+              "&api_key=" + apiKey +"&tags=" + tags + "&min_taken_date=" + minDate + 
+              "&max_taken_date=" + maxDate + "&lat=" + lat + "&lon=" + lng +
+              "&radius=" + radius + "&tag_mode=all&sort=date-taken-desc&extras=date_taken" +
+              "&per_page=200";
+*/
+
+  }
+
+  search();
 //  $.getJSON('
 });
