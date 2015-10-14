b30f896e0639e36f1cde3e2f23e84bd712937c25
Damon <damon.ho@student.unsw.edu.au>
2015-10-14 21:24:49 +1100

    Added Calendar to slider

--- static/search/js/slider.js
+++ static/search/js/slider.js
@@ -8,12 +8,26 @@ $("#slider").slider({
     slide: function(event, ui) {
         var delay = function() {
             var handleIndex = $(ui.handle).index();
-            var label = handleIndex == 3 ? '#min' : '#max';
+            var label = handleIndex == 1 ? '#min' : '#max';
+            console.log(handleIndex);
+            var timePicker = label == '#min' ? '#minDate' : '#maxDate';
             $(label).html("01 Jan " + (1850 + ui.value)).position({
                 my: 'center bottom-15',
                 at: 'center top',
                 of: ui.handle,
                 offset: "0, 10"
+            });
+            $(timePicker).stop(true,false).fadeTo(100,1,function() {
+                $(this).mouseover(function() {
+                    //console.log("Mouse over");        
+                    $(this).stop(true,true).fadeTo(100,1); 
+                });
+            }).delay(2000).fadeTo(1000,0,function() {
+                $(this).unbind('mouseover').datepicker("hide");
+            }).datepicker("setDate","1/1/"+(1850+ui.value)).position({
+                my: 'center bottom',
+                at: 'center bottom',
+                of: $(label)
             });
         };
         
@@ -21,7 +35,44 @@ $("#slider").slider({
         setTimeout(delay, 5);
     }
 });        
-                            
+ 
+$('#minDate').datepicker({
+    changeMonth: true,
+}).datepicker("setDate","1/1/1850").position({
+    my: 'center bottom',
+    at: 'center bottom',
+    of: $("#min")
+    }).mouseover(function() {
+        //console.log("Mouse over");        
+       $(this).stop(true,true).fadeTo(100,1); 
+    }).mouseleave(function() {
+       //console.log("Mouse gone");
+       $(this).delay(2000).fadeTo(1000,0,function() {
+           $(this).unbind('mouseover').datepicker("hide");
+       }); 
+    }).delay(2000).fadeTo(1000,0,function() {
+           $(this).unbind('mouseover').datepicker("hide");
+       });
+
+       
+
+$('#maxDate').datepicker({
+    changeMonth: true    
+}).position({
+    my: 'center bottom',
+    at: 'center bottom',
+    of: $("#max")
+    }).mouseover(function() {
+        //console.log("Mouse over");        
+       $(this).stop(true,true).fadeTo(100,1); 
+    }).mouseleave(function() {
+       //console.log("Mouse gone");
+       $(this).delay(2000).fadeTo(1000,0,function() {
+           $(this).unbind('mouseover').datepicker("hide");
+       }); 
+    }).delay(2000).fadeTo(1000,0,function() {
+           $(this).unbind('mouseover').datepicker("hide");
+       });                           
 $('#min').text("01 Jan " + (1850+$("#slider").slider("values",0))).position({
     my: 'center bottom-15',
     at: 'center top',
@@ -33,3 +84,5 @@ $('#max').text(moment().format('DD MMM YYYY')).position({
     at: 'center top',
     of: $(".ui-slider-handle")[1]
 });
+
+
