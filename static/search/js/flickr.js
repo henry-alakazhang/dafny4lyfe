79c21fe4e3b78985d306943ba8d0d7c779fd91df
Benjamin Mo <b.mo96@hotmail.com>
2015-10-17 21:09:44 +1100

    contentflow working, changed JS to accept distance as m instead of km

--- static/search/js/flickr.js
+++ static/search/js/flickr.js
@@ -1,9 +1,10 @@
 jQuery(function() {
   // Flickr Key
   var apiKey = 'd924f5ea2a765922fc8794b3f9942133';
+  // ContentFlow for images
+  var ajax_cf = new ContentFlow('ajax_cf');
 
-  jQuery('<img alt="" />').attr('id', 'loader').attr('src', 'ajax-loader.gif').appendTo('#image-container');
-
+  // returns the value of the specified query parameter
   function getQueryVar(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
@@ -26,12 +27,12 @@ jQuery(function() {
       max_taken_date: getQueryVar('max_date'),
       lat: getQueryVar('lat'),
       lon: getQueryVar('lng'),
-      radius: getQueryVar('dist'),
+      radius: (getQueryVar('dist')/1000),
       tag_mode: 'all',
       sort: 'date-taken-desc',
       extras: 'date_taken',
       format: 'json',
-      per_page: '200',
+      per_page: '500',
       page: '1'
     }
 
@@ -39,18 +40,31 @@ jQuery(function() {
     search();
 
     function search() {
+      // Performs request using API (REST method) to flickr
       jQuery.getJSON(baseUrl, flickrOptions, function(data) {
-        console.log(data.photos.photo.length);
         if (data.photos.photo.length == 0 && flickrOptions['page'] == 1) {
-          // display no photos
+          // display no photos and do stuff to show no that there are no results
         } else {
-          jQuery.each(data.photos.photo, function(index, item) {
-            var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + 
-                           item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
-            console.log(photoURL);
-            var imgCont = "<figure> <img src='" + photoURL + "'></figure>";
-            jQuery(imgCont).appendTo('#image-container');
+          // create the DOM element for the contentflow
 
+          // for each photo object returned
+          jQuery.each(data.photos.photo, function(index, item) {
+            var photoUrl = 'http://farm' + item.farm + '.static.flickr.com/' + 
+                           item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
+//            console.log(photoUrl);
+            var divDom = document.createElement("DIV");
+            divDom.setAttribute("class", "item");
+            var imgDom = document.createElement("IMG");
+            imgDom.setAttribute("class", "content");
+
+            imgDom.src = photoUrl;
+            divDom.appendChild(imgDom);
+            
+            ajax_cf.addItem(divDom, 'last');
+
+            // once all the items from current page have been returned
+            // get items from next page
+            // NEED TO CHANGE TO next page on end of scroll... else will load 4eva
             if (++count == data.photos.photo.length) {
               flickrOptions['page']++;
               count = 0;
