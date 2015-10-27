38eabee03b7f7eb403b59e05f0ed668419b3c44b
Kai Pridham <william@pridham.net.au>
2015-10-27 16:51:33 +1100

    Added virtual environment. Redirect from root to /search/map.

--- dafny4lyfe/urls.py
+++ dafny4lyfe/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponsePermanentRedirect
+
+def redirect(url):
+    return HttpResponsePermanentRedirect('/search/map')
 
 urlpatterns = [
+    url(r'^$', redirect),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^search/', include('search.urls', namespace="search")),
 ]
