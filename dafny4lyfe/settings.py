446bc4e093408a38652d09f6d87894f6f986f19f
Benjamin Mo <b.mo96@hotmail.com>
2015-12-25 17:57:43 +1100

    another try for heroku deploy

--- dafny4lyfe/settings.py
+++ dafny4lyfe/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = '/static/'
-STATIC_URL = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'search')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
